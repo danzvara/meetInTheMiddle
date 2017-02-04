43a369b404361fda27c09769bf3b09a9ddeec3ca
Daniel Zvara <dan.zvara@gmail.com>
2017-02-04 21:24:38 +0000

    flight: flight container
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

---
+++ src/flight.py
@@ -0,0 +1,9 @@
+class Flight:
+  def __init__(self, country, currency, locale, origin, dest, outtime, intime):
+    self.country = country
+    self.currency = currency
+    self.locale = locale
+    self.origin = origin
+    self.dest = dest
+    self.outtime = outtime
+    self.intime = intime
