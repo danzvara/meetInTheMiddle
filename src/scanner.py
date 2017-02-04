0668e8ab6340dcaf07c87992a981c12d296c35a9
Daniel Zvara <dan.zvara@gmail.com>
2017-02-04 20:24:35 +0000

    scanner: get flights
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

--- src/scanner.py
+++ src/scanner.py
@@ -6,7 +6,7 @@ class Scanner:
     pass
 
   def _request(self):
-    url = "http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/{country}/{currency}/{locale}/EDI/LHR/anytime/anytime?apiKey=prtl6749387986743898559646983194"
+    url = "http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/uk/gbp/en-GB/EDI/LHR/anytime/anytime?apiKey=prtl6749387986743898559646983194"
     response = requests.get(url)
     print(response.json())
 
