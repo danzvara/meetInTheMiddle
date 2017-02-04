562f5f02bde20539e2252f1df3fc721d5f02497f
Daniel Zvara <dan.zvara@gmail.com>
2017-02-04 21:24:09 +0000

    scanner: parametrize
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

--- src/scanner.py
+++ src/scanner.py
@@ -1,15 +1,18 @@
 import requests
+import json
 
 class Scanner:
 
   def __init__(self):
     pass
 
-  def _request(self):
-    url = "http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/uk/gbp/en-GB/EDI/LHR/anytime/anytime?apiKey=prtl6749387986743898559646983194"
+  def _request(self, country, currency, locale, origin,
+              dest, outtime, intime):
+    apiKey = "prtl6749387986743898559646983194"
+    url = "http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0"
+    url += ("/" + country + "/" + currency + "/" + locale + "/"
+           + origin + "/" + dest + "/" + outtime + "/" + intime + "?apiKey=" +
+           apiKey)
     response = requests.get(url)
-    print(response.json())
-
-
-scanner = Scanner()
-scanner._request()
+    json_res = response.json()
+    return json_res
