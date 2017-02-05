f55087d9d001af298189c7ca51ecd70067b8afc2
Daniel Zvara <dan.zvara@gmail.com>
2017-02-05 02:14:35 +0000

    Meet: filter results
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

--- meetInTheMiddle/Meet.py
+++ meetInTheMiddle/Meet.py
@@ -1,5 +1,7 @@
 import scanner
+from itertools import product
 import flight
+import time
 
 class Meet:
 
@@ -13,32 +15,57 @@ class Meet:
     res = {}
     res["MinPrice"] = jobj["MinPrice"]
     res["Destination"] = jobj["OutboundLeg"]["DestinationId"]
+    res["OutTime"] = jobj["OutboundLeg"]["DepartureDate"][0:10]
+    res["InTime"] = jobj["InboundLeg"]["DepartureDate"][0:10]
 
     return res
 
-  def _get_flight(self, flightA, flightB):
+  def _searchForFlight(self, flightA, flightB):
     sky = scanner.Scanner()
     flightsDano = sky._request(flightA.country, flightA.currency,
                               flightA.locale, flightA.origin, flightA.dest,
-                              flightA.outtime, flightA.intime)["Quotes"]
+                              flightA.outtime, flightA.intime)
     flightsBrch = sky._request(flightB.country, flightB.currency,
                               flightB.locale, flightB.origin, flightB.dest,
-                              flightB.outtime, flightB.intime)["Quotes"]
+                              flightB.outtime, flightB.intime)
+    print(flightsDano)
+    airportsDano = filter((lambda x: x["Type"] == "Station"), flightsDano["Places"])
+    airportsBrch = filter((lambda x: x["Type"] == "Station"), flightsBrch["Places"])
+    airports = airportsBrch + airportsDano
+    airports_ = {}
+    for x in airports:
+      airports_[x["PlaceId"]] = {"IataCode":x["IataCode"], "Name":x["Name"]}
+    print(airports_)
+
+    flightsBrch = flightsBrch["Quotes"]
+    flightsDano = flightsDano["Quotes"]
     cleanDano = map(self._clean, flightsDano)
     cleanBrch = map(self._clean, flightsBrch)
 
-    common = filter(lambda x: x["Destination"] in
-        map((lambda x: x["Destination"]), cleanBrch), cleanDano)
+    pairs = list(product(cleanBrch, cleanDano))
+    criteria = ["Destination", "OutTime"]
+    for crit in criteria:
+      pairs = filter((lambda x: x[0][crit] == x[1][crit]), pairs)
 
-    print(common)
-    return common
+    for p in pairs:
+      p[0]["Destination"] = airports_[p[0]["Destination"]]["Name"]
+      p[1]["Destination"] = p[0]["Destination"]
 
-    #bestFlights = _chooseBest(flightsDano, flightsBrch)
+    return pairs
+
+  def get_flight(self, flightA, flightB):
+    return self._searchForFlight(flightA, flightB)
+
+
+
+
 
 def main():
   meet = Meet()
-  flightA = flight.Flight("uk", "gbp", "en-GB", "KSC", "everywhere", "anytime", "anytime")
-  flightB = flight.Flight("uk", "gbp", "en-GB", "BTS", "everywhere", "anytime", "anytime")
+  flightA = flight.Flight("uk", "gbp", "en-GB", "VIE", "everywhere",
+      "2017-04-05", "2017-04-10")
+  flightB = flight.Flight("uk", "gbp", "en-GB", "BTS", "everywhere",
+      "2017-04-05", "2017-04-10")
 
   meet._get_flight(flightA, flightB)
 
