9027efcea775d9174121c7a342bd09e325f4c33e
Daniel Zvara <dan.zvara@gmail.com>
2017-02-05 05:43:03 +0000

    views: speed up first load
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

--- meetInTheMiddle/views.py
+++ meetInTheMiddle/views.py
@@ -8,12 +8,6 @@ meet = Meet()
 
 @app.route('/')
 def index():
-    flightA = flight.Flight("uk", "gbp", "en-GB", "STN", "everywhere",
-        "2017-03-27", "2017-04-04")
-    flightB = flight.Flight("uk", "gbp", "en-GB", "BTS", "everywhere",
-        "2017-03-27", "2017-04-04")
-
-    print(json.dumps(meet.get_flight(flightA, flightB)))
     return render_template('hello.html', name='jano')
 
 @app.route('/api/request', methods=['GET'])
